b8e13060b0c42bc86d6cd50084963bf7dfaa50d7
manu-msr <manu@ciencias.unam.mx>
2016-04-09 11:05:09 -0500

    Agrego intento de registro.

---
+++ IOteayudo/src/java/controlador/AltaUsuario.java
@@ -0,0 +1,135 @@
+package controlador;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import modelo.Usuario;
+
+/**
+ * Controlador que permite agregar un usuario a la base de datos.
+ * @author Manuel Soto Romero.
+ * @version 1.0
+ */
+@ManagedBean
+@RequestScoped
+public class AltaUsuario {
+    
+    /* DATOS SUFICIENTES PARA DAR DE ALTA A UN USUARIO. */
+    
+    /* ID del usuario. */
+    private int id;
+    /* Correo electrónico del usuario. */
+    private String correo;
+    /* Contraseña del usuario. */
+    private String contrasenia;
+    /* Confirmación de la contraseña. */
+    private String confirmacion;
+    /* Nombre del usuario. */
+    private String nombre;
+    /* Apellido paterno del usuario. */
+    private String apellidop;
+    /* Apellido materno del usario. */
+    private String apellidom;
+    /* Nos indica si es alumno o tutor. */
+    private boolean esAlumno;
+    /* Obtiene información de las peticiones. */
+    private final HttpServletRequest httpServletRequest;
+    /* Obtiene información de la aplicación. */
+    private final FacesContext faceContext;
+    /* Permite el envio de mensajes entre el bean y la vista. */
+    private FacesMessage message;
+    
+    /**
+     * Constructor por omisión.
+     * Inicializa los objetos necesarios para comunicarnos con la vista.
+     */
+    public AltaUsuario() {
+        faceContext = FacesContext.getCurrentInstance();
+        httpServletRequest = 
+                (HttpServletRequest)faceContext.getExternalContext().getRequest();
+    }
+    
+    /**
+     * Da de alta al usuario y lo redirige a su perfil.
+     * @return Dirección de la vista perfil.
+     */
+    public String darDeAlta() {
+        if (contrasenia.equals(confirmacion)) {
+            Usuario us = new Usuario();
+            // algo ...
+        } else {
+            message = new FacesMessage(FacesMessage.SEVERITY_ERROR, 
+                    "Las contraseñas no coinciden", null);
+            faceContext.addMessage(null, message);
+            return "#";
+        }
+        return "#";
+    }
+    
+    /* MÉTODOS DE MODIFICADORES Y DE ACCESO PARA COMUNICARNOS CON LA VISTA */
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getConfirmacion() {
+        return confirmacion;
+    }
+
+    public void setConfirmacion(String confirmacion) {
+        this.confirmacion = confirmacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidop() {
+        return apellidop;
+    }
+
+    public void setApellidop(String apellidop) {
+        this.apellidop = apellidop;
+    }
+
+    public String getApellidom() {
+        return apellidom;
+    }
+
+    public void setApellidom(String apellidom) {
+        this.apellidom = apellidom;
+    }
+
+    public boolean isEsAlumno() {
+        return esAlumno;
+    }
+
+    public void setEsAlumno(boolean esAlumno) {
+        this.esAlumno = esAlumno;
+    }
+}
